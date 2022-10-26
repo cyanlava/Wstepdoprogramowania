c627bad0fa3c3bf437b1d5577e0318c0cc8ad3c4
cyanlava <109446821+cyanlava@users.noreply.github.com>
2022-10-26 14:01:06 +0200

    Update zad12.py

--- zad12.py
+++ zad12.py
@@ -1,13 +1,19 @@
 m = [6, 5, 46, 3, 754, 243]
 n = [-3, -2, -9, -1]
 
-j = n[:1]
-print(j)
 
-min = 0
-max = 0
+
+
+max = m[0]
 for i in m:
     if i > max:
         max = i
 
 print(max)
+
+min = n[0]
+for i in n:
+    if i < min:
+        min = i
+
+print(min)
