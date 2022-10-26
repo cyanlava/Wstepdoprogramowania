a0fbe6cc1021bc2fc1570e80ce463d5ea0cb00f0
cyanlava <109446821+cyanlava@users.noreply.github.com>
2022-10-26 14:23:34 +0200

    Update zad13.py

--- zad13.py
+++ zad13.py
@@ -1,3 +1,12 @@
-x = 5
+x = 7568723
+m = []
+for i in range(2, x):
+    if x % i == 0:
+        m.append(i)
 
-for
+
+
+if len(m) == 0:
+    print("liczba pierwsza")
+else:
+    print("nie jest to liczba pierwsza")
