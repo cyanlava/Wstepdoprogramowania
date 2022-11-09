dc62e9fdaa6ce641023581da3ab995aadbede90a
cyanlava <prywint@proton.me>
2022-11-10 00:29:58 +0100

    do it

---
+++ zad12.py
@@ -0,0 +1,9 @@
+from random import seed
+from random import randint
+
+#ziarno losowosci, mozna wpisac dowolny numer
+seed(193)
+
+#losowa liczba calkowita z przedzialu od 0 do 10
+x = randint(0, 10)
+print(x)
