f40341726cb73c005c61fe5f99140c50a59d85dc
cyanlava <prywint@proton.me>
2022-11-14 18:23:22 +0100

    polishing touches

--- lab5/zad1.py
+++ lab5/zad1.py
@@ -1,8 +1,19 @@
-n = 2
-m = 3
+print("podaj wymiary matrycy n i m: ")
+x = input("pamiętaj by oddzielić liczby spacją :) ")
+list = x.split(" ")
+n = int(list[0])
+m = int(list[1])
+
+
+
 from random import seed 
 from random import randint
 seed(123)
+
+print("wersja podstawowa: ")
+
+#tworzenie podstawowej matrycy a
+#wymiary matrycy mozna zmienic dowolnie przez n i m
 a = []
 for i in range(n):
     a.append([0] * m)
@@ -10,20 +21,12 @@ for i in range(n):
         a[i][j] = randint(0, 20)
     print(a[i])
 
-
+print(" wersja transponowana: ")
  
-
+#tworzenie transponowanej matrycy at 
 at=[]
-print(len(a[0]))
 for i in range(len(a[0])):
     at.append([0]*len(a))
     for j in range(len(a)):
         at[i][j] = a[j][i]
     print(at[i])
-
-'''
-for i in range(len(a[0])):
-    for j in range(len(a)):
-        at[i][j] = a[j][i]
-    print(at[i])
-'''
