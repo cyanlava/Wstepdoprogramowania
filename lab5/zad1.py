4f442e7da0ef8f1944d9bd0fe1ce33cd277809d4
cyanlava <prywint@proton.me>
2022-11-14 17:06:15 +0100

    lab5 first commit

---
+++ lab5/zad1.py
@@ -0,0 +1,19 @@
+n = 2
+m = 3
+from random import seed 
+from random import randint
+seed(123)
+a = []
+for i in range(n):
+    a.append([0] * m)
+    for j in range(m):
+        a[i][j] = randint(0, 20)
+    print(a[i])
+
+at=[]
+print(len(a[0]))
+for i in range(len(a[0])):
+    at.append([0]*len(a))
+    for j in range(len(a)):
+        at[i][j]=0
+    print(at[i])
